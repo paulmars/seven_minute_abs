50895e777a7dff0c38b9e0843894dff579f01a46
Paul <paul.mckellar@gmail.com>
2009-03-19 16:58:46 -0700

    much similar version, complete install docs

--- lib/ab_view_helper.rb
+++ lib/ab_view_helper.rb
@@ -5,24 +5,22 @@ module AbViewHelper
   #     <%= link_to "Blue version", "new", {:style => "color:blue"} %>
   #   <% elsif version == 1 %>
   #     <%= link_to "Orange version", "new?ab=#{test.stub}", {:style => "color:orange"} %>
-  #   <% else %>
-  #     <%= link_to "Orange version", "new", {:style => "color:orange"} %>
   #   <% end %>
   # <% end %>
 
   def ab_test(testname, version_count, &block)
     testname = testname.gsub(/\W/,'_')
-    ab = Ab.for_testname(testname, version_count, @ab_version)
+    ab = Ab.for_testname(testname, version_count, (@ab_version || Time.now.to_i))
     version = ab ? ab.version : nil
     content_tag = capture(ab, version, &block)
-    block_is_within_action_view?(block) ? concat(content_tag, block.binding) : content_tag
+    block_called_from_erb?(block) ? concat(content_tag) : content_tag
   end
 
-  def click_count(testname, &block)
-    testname = "click_count:#{testname.gsub(/\W/,'_')}"
-    ab = Ab.for_click_count(testname, @inc_ab_display_count)
-    content_tag = capture(ab, &block)
-    block_is_within_action_view?(block) ? concat(content_tag, block.binding) : content_tag
-  end
+  # def click_count(testname, &block)
+  #   testname = "click_count:#{testname.gsub(/\W/,'_')}"
+  #   ab = Ab.for_click_count(testname, @inc_ab_display_count)
+  #   content_tag = capture(ab, &block)
+  #   block_called_from_erb?(block) ? concat(content_tag) : content_tag
+  # end
 
 end
--- lib/seven_minute_abs.rb
+++ lib/seven_minute_abs.rb
@@ -3,18 +3,19 @@ module SevenMinuteAbs
   def ab_click_count
     Ab.click!(params[:ab]) if params[:ab]
   end
-
-  # sets up random string, mod by version available
-  def ab_version_modulation
-    @ab_version ||= Time.now.to_i
-  end
-
-  def increment_display_count?
-    @inc_ab_display_count = true
-  end
-
-  def skip_display_count!
-    @inc_ab_display_count = false
-  end
+  
+  # future tutorials
+  
+  # def ab_version_modulation
+  #   @ab_version ||= Time.now.to_i
+  # end
+  # 
+  # def increment_display_count?
+  #   @inc_ab_display_count = true
+  # end
+  # 
+  # def skip_display_count!
+  #   @inc_ab_display_count = false
+  # end
 
 end
